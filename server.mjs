baa90abbdbf84e24dae4cac60f172ff5125cb066
SmyslovAG <a.smyslov@avia.mecom.ru>
2024-11-11 17:52:23 +0300

    init: create project

---
+++ server.mjs
@@ -0,0 +1,28 @@
+import { Server } from 'node:http'
+import fs from 'node:fs/promises'
+
+const port = 3000
+const server = new Server()
+server.listen(port)
+
+server.on('request', async (request, response) => {
+  if (request.url === '/') {
+    const fileHandle = await fs.open('views/index.html')
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html')
+    fileHandle.createReadStream().pipe(response)
+  } else if (request.url === '/css/main.css') {
+    const fileHandle = await fs.open('public/css/main.css')
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/css')
+    fileHandle.createReadStream().pipe(response)
+  } else {
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/plain')
+    response.end('Error 404: Page Not Found')
+  }
+})
+
+server.on('listening', () => {
+  console.log(`server: start listen on port ${ port }`)
+})
