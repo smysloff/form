501a3c2a1795c2b007b85df0d1c0c59cf7da2baa
SmyslovAG <a.smyslov@avia.mecom.ru>
2024-11-13 17:59:55 +0300

    wip

--- public/js/main.mjs
+++ public/js/main.mjs
@@ -4,39 +4,62 @@ class MetarSpeciReport {
 
     this.form = formElement
     this.output = this.form.elements['output']
+    this.report = {}
 
-    this.report = {
-      type: this.getType(),
-      icao: this.getICAO(),
-    }
-    this.updateOutput()
+    this.updateType()
+    this.updateICAO()
+    this.updateDate()
+    this.updateTime()
 
     for (const element of this.form.elements['type']) {
-      element.addEventListener('change', (e) => {
-        this.report.type = this.getType()
-        this.updateOutput()
-      })
+      element.addEventListener('change', () => this.updateType())
     }
 
-    this.form.elements['icao'].addEventListener('input', (e) => {
-      e.target.value = e.target.value.toUpperCase()
-      this.report.icao = this.getICAO()
-      this.updateOutput()
-    })
+    this.form.elements['icao']
+      .addEventListener('input', () => this.updateICAO())
+
+    this.form.elements['date']
+      .addEventListener('input', () => this.updateDate())
+
+    this.form.elements['time']
+      .addEventListener('input', () => this.updateTime())
   }
 
-  getType() {
-    return this.form.querySelector(
+
+  updateType() {
+    const element = this.form.querySelector(
       'input[type="radio"][name="type"]:checked')
-        .value.toUpperCase()
+    this.updateReport('type', element.value.toUpperCase())
   }
 
-  getICAO() {
-    return this.form.elements['icao'].value
+  updateICAO() {
+    const element = this.form.elements['icao']
+    element.value = element.value.toUpperCase()
+    this.updateReport('icao', element.value)
   }
 
-  updateOutput() {
-    const output = `${ this.report.type } ${ this.report.icao }`
+  updateDate() {
+    const element = this.form.elements['date']
+    element.value = element.value.replace(/[^\d\.]/g, '')
+    this.updateReport('date', element.value.split('.')[0])
+  }
+
+  updateTime() {
+    const element = this.form.elements['time']
+    element.value = element.value.replaceAll(/[^\d\:]/g, '')
+    const parts = element.value.split(':')
+    this.updateReport('time', `${ parts[0] }${ parts[1] }`)
+  }
+
+  updateReport(key, value) {
+    this.report[key] = value ?? ''
+    const output = ''
+            + this.report.type
+      + ' ' + this.report.icao
+      + ' ' + this.report.date + (this.report.time + 'Z')
+      + ''
+    console.log(this.report)
+    console.log(output)
     this.output.value = output
   }
 
